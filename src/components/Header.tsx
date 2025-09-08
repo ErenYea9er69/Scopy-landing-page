cde47a4126c07db4f13ab0a0618d93e73e1327b2
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-09-08 21:55:21 +0100

    not bad button on the header...

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ const Header: React.FC = () => {
           <img
             src="/assets/pill.png"
             alt="Get Started Button"
-            className="h-20 w-45 object-contain brightness-110 contrast-125"
+            className="h-20 w-45 object-contain brightness-130 contrast-100"
           />
           <div className="absolute inset-0 flex items-center justify-center">
             <span className="text-black text-lg font-medium whitespace-nowrap">
