f85deb442e6d961e386b61effb64d43eecabcf87
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-09-21 15:03:00 +0100

    deleted testimonials page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Background from './components/Background';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Pricing from './components/Pricing';
-import Testimonials from './components/Testimonials';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 
@@ -35,8 +34,7 @@ const App = () => {
         <Hero />
         <Features />
          <Pricing />
-         <Testimonials />
-        <CTA />
+         <CTA />
       </main>
       <Footer />
     </div>
